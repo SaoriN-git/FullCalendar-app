99b90b33141d1d001d2948aafe3294150e431168
Saori Nishimitsu <sa.nishimitsu@toi-iot.net>
2024-07-02 19:58:17 +0900

    5.登録したスケジュールの表示

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -3,13 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Models\Schedule;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Facades\Auth;
 
 class ScheduleController extends Controller
 {
   public function index()
   {
-    return view('schedules.index');
+    $schedules = Schedule::all();
+    $events = [];
+    foreach ($schedules as $schedule) {
+      $events[] = [
+        'id' => $schedule->id,
+        'title' => $schedule->title,
+        'description' => $schedule->description,
+        'start' => $schedule->start_date,
+        'end' => Carbon::parse($schedule->end_date)->addDay()->format('Y-m-d'),
+      ];
+    }
+    return view('schedules.index', compact('events'));
   }
 
   public function create()
