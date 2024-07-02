f4402e0baa3cd39dd859d37ebf90e453f7ac2b79
Saori Nishimitsu <sa.nishimitsu@toi-iot.net>
2024-07-02 19:40:13 +0900

    4.FullCalendarの導入

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class ScheduleController extends Controller
 {
+  public function index()
+  {
+    return view('schedules.index');
+  }
+
   public function create()
   {
     return view('schedules.create');
---
+++ resources/views/schedules/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">スケジュール</div>
+
+                <div class="card-body">
+									<div id="calendar"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('script')
+<script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.14/index.global.min.js'></script>
+<script>
+
+	document.addEventListener('DOMContentLoaded', function() {
+		var calendarEl = document.getElementById('calendar');
+		var calendar = new FullCalendar.Calendar(calendarEl, {
+			initialView: 'dayGridMonth'
+		});
+		calendar.render();
+	});
+
+</script>
+@endsection
