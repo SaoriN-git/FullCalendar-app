28db6d8b528ff17ba548820b31dde122436a386b
Saori Nishimitsu <sa.nishimitsu@toi-iot.net>
2024-07-03 18:41:07 +0900

    9.ポップアップの設定

--- resources/views/schedules/index.blade.php
+++ resources/views/schedules/index.blade.php
@@ -18,18 +18,20 @@
 
 @section('script')
   <style>
-		.fc-col-header-cell-cushion,
-		.fc-daygrid-day-number {
-			color: #333;
-			text-decoration: none;
-		}
-		.fc-col-header-cell.fc-day-sat {
-			background-color: #9ecef5;
-		}
-		.fc-col-header-cell.fc-day-sun {
-			background-color: #f4d0df;
-		}
-	</style>
+    .fc-col-header-cell-cushion,
+    .fc-daygrid-day-number {
+      color: #333;
+      text-decoration: none;
+    }
+
+    .fc-col-header-cell.fc-day-sat {
+      background-color: #9ecef5;
+    }
+
+    .fc-col-header-cell.fc-day-sun {
+      background-color: #f4d0df;
+    }
+  </style>
   <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.14/index.global.min.js'></script>
   <script>
     document.addEventListener('DOMContentLoaded', function() {
@@ -39,24 +41,34 @@
         initialView: 'dayGridMonth',
         events: events,
         locale: 'ja',
-				height: '500px',
-				firstDay: 1, // 0:日曜～6：土曜日
-				headerToolbar: {
-					left: "dayGridMonth, timeGridWeek, timeGridDay, listWeek",
-					center: "title",
+        height: '500px',
+        firstDay: 1, // 0:日曜～6：土曜日
+        headerToolbar: {
+          left: "dayGridMonth, timeGridWeek, timeGridDay, listWeek",
+          center: "title",
           right: "today prev,next"
-				},
-				buttonText: {
-					today: '今日',
-					month: '月',
-					week: '週',
-					day: '日',
-					list: 'リスト',
-				},
-				noEventsContent: 'スケジュールはありません',
-				fixedWeekCount: false, // 6週目を表示する場合は'true'に設定
-				showNonCurrentDates: false, // 当月以外はグレー表示
-				// initialDate: '2023-04-01', // 初期表示する年月を指定する
+        },
+        buttonText: {
+          today: '今日',
+          month: '月',
+          week: '週',
+          day: '日',
+          list: 'リスト',
+        },
+        noEventsContent: 'スケジュールはありません',
+        fixedWeekCount: false, // 6週目を表示する場合は'true'に設定
+        showNonCurrentDates: false, // 当月以外はグレー表示
+        // initialDate: '2023-04-01', // 初期表示する年月を指定する
+        eventMouseEnter(info) {
+          $(info.el).popover({
+            title: info.event.title,
+            content: info.event.extendedProps.description,
+            trigger: 'hover',
+            placement: 'top',
+            container: 'body',
+            html: true
+          });
+        },
       });
       calendar.render();
     });
